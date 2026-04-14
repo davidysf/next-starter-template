87c068ae18e73590d924a117f2c003e66aa9d7ac
davidysf <duduyossaf@gmail.com>
2026-04-15 00:13:01 +0300

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,20 +1,24 @@
-// open-next.config.ts
 const config = {
   default: {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
+      proxyExternalRequest: "fetch",
       incrementalCache: "dummy",
       tagCache: "dummy",
       queue: "dummy",
     },
   },
+  edgeExternals: ["node:crypto"],
   middleware: {
     external: true,
     override: {
       wrapper: "cloudflare-edge",
       converter: "edge",
       proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
     },
   },
 };
